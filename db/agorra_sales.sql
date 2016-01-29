34eef6ac0f6086b9dd9337b4477272321c540a13
goygoybords <kevinseankho@yahoo.com>
2016-01-29 16:00:23 +0800

    1/29/2016 DB afternoon push

--- db/agorra_sales.sql
+++ db/agorra_sales.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 29, 2016 at 12:59 PM
+-- Generation Time: Jan 29, 2016 at 03:59 PM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `clients` (
 --
 
 INSERT INTO `clients` (`client_id`, `company_name`, `company_address`, `contact_person`, `contact_number`, `email`, `credit_limit`, `balance`, `status`) VALUES
-(1, 'Flashpark Inc', 'Mandaue City North Atrium', 'Hiro Takahashi', '199232323', 'flashpark@gmail.com', '100000.00', '0.00', 1);
+(1, 'Flashpark Inc.', 'Mandaue City', 'Yuki Takahashi', '09232323212', 'flashpark@gmail.com', '100000.00', '0.00', 1);
 
 -- --------------------------------------------------------
 
@@ -67,9 +67,12 @@ INSERT INTO `migrations` (`migration`, `batch`) VALUES
 ('2014_10_12_100000_create_password_resets_table', 1),
 ('2016_01_26_013157_create_sessions_table', 1),
 ('2016_01_27_012016_create_clients_tbl', 1),
+('2016_01_27_053758_create_proposal_attachment_tbl', 1),
 ('2016_01_27_063552_create_proposal_tbl', 1),
 ('2016_01_27_071810_create_service_category_list', 1),
-('2016_01_28_002552_create_proposalDetail_tbl', 1);
+('2016_01_28_002552_create_proposalDetail_tbl', 1),
+('2016_01_29_053739_create_sales_attachment_tbl', 1),
+('2016_01_29_053840_create_sales_tbl', 1);
 
 -- --------------------------------------------------------
 
@@ -98,19 +101,20 @@ CREATE TABLE IF NOT EXISTS `proposals` (
   `salesperson` int(10) unsigned NOT NULL,
   `client_id` int(10) unsigned NOT NULL,
   `total` decimal(8,2) unsigned NOT NULL,
-  `file` blob NOT NULL,
+  `proposal_attachment_id` int(10) unsigned NOT NULL,
   `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`proposal_id`),
   KEY `proposals_salesperson_foreign` (`salesperson`),
-  KEY `proposals_client_id_foreign` (`client_id`)
+  KEY `proposals_client_id_foreign` (`client_id`),
+  KEY `proposals_proposal_attachment_id_foreign` (`proposal_attachment_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `proposals`
 --
 
-INSERT INTO `proposals` (`proposal_id`, `project_name`, `proposal_date`, `salesperson`, `client_id`, `total`, `file`, `status`) VALUES
-(1, 'RingRob Project', '2016-01-28', 1, 1, '100000.00', 0x2f6f70742f6c616d70702f74656d702f7068707536717a646f, 1);
+INSERT INTO `proposals` (`proposal_id`, `project_name`, `proposal_date`, `salesperson`, `client_id`, `total`, `proposal_attachment_id`, `status`) VALUES
+(1, 'RingRob Project', '2016-01-29', 1, 1, '100000.00', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -126,15 +130,72 @@ CREATE TABLE IF NOT EXISTS `proposals_detail` (
   PRIMARY KEY (`proposal_detail_id`),
   KEY `proposals_detail_proposal_id_foreign` (`proposal_id`),
   KEY `proposals_detail_service_category_id_foreign` (`service_category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `proposals_detail`
 --
 
 INSERT INTO `proposals_detail` (`proposal_detail_id`, `proposal_id`, `service_category_id`, `status`) VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 1);
+(1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proposal_attachment`
+--
+
+CREATE TABLE IF NOT EXISTS `proposal_attachment` (
+  `proposal_attachment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `filetype` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `file` blob NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`proposal_attachment_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `proposal_attachment`
+--
+
+INSERT INTO `proposal_attachment` (`proposal_attachment_id`, `filename`, `filetype`, `file`, `status`) VALUES
+(1, 'price.pdf', 'application/pdf', 0x2f6f70742f6c616d70702f74656d702f706870654739696e59, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE IF NOT EXISTS `sales` (
+  `sales_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sales_date` date NOT NULL,
+  `proposal_id` int(10) unsigned NOT NULL,
+  `sale_attachment_id` int(10) unsigned NOT NULL,
+  `terms` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `isVatable` tinyint(1) NOT NULL,
+  `isCommisionable` tinyint(1) NOT NULL,
+  `total` decimal(8,2) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`sales_id`),
+  KEY `sales_proposal_id_foreign` (`proposal_id`),
+  KEY `sales_sale_attachment_id_foreign` (`sale_attachment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sale_attachment`
+--
+
+CREATE TABLE IF NOT EXISTS `sale_attachment` (
+  `sale_attachment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `filetype` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `file` blob NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`sale_attachment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -203,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `type`, `status`) VALUES
-(1, 'Filjumar Jumamoy', 'fil@gmail.com', '$2y$10$BexGpbyL5q7EohsqjUBGW.Ur1c8yFsXlKDhUU7P3QzIxEDJAvKhRO', 'tcd7Va0kjwhmnC2vOtILXW53E7YE1s112wNhX8RR8oq0DTHSh8WdDEru1jS0', '2016-01-29 01:25:18', '2016-01-28 17:25:18', 2, 1);
+(1, 'Filjumar Jumamoy', 'fil@gmail.com', '$2y$10$TC6oZ/te9DDga3t8OVAlsuBBZA6N0AlEHXRJrAkno/.EIEHUllTga', '3cYZNhJUgCx6aeYfdUmZu8n9ApBhlR4xO9FJQGNp2kQTgFYRd2P6UUofkY9d', '2016-01-29 07:55:40', '2016-01-28 23:55:40', 2, 1);
 
 --
 -- Constraints for dumped tables
@@ -213,6 +274,7 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `creat
 -- Constraints for table `proposals`
 --
 ALTER TABLE `proposals`
+  ADD CONSTRAINT `proposals_proposal_attachment_id_foreign` FOREIGN KEY (`proposal_attachment_id`) REFERENCES `proposal_attachment` (`proposal_attachment_id`),
   ADD CONSTRAINT `proposals_client_id_foreign` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`),
   ADD CONSTRAINT `proposals_salesperson_foreign` FOREIGN KEY (`salesperson`) REFERENCES `users` (`id`);
 
@@ -220,8 +282,15 @@ ALTER TABLE `proposals`
 -- Constraints for table `proposals_detail`
 --
 ALTER TABLE `proposals_detail`
-  ADD CONSTRAINT `proposals_detail_proposal_id_foreign` FOREIGN KEY (`proposal_id`) REFERENCES `proposals` (`proposal_id`),
-  ADD CONSTRAINT `proposals_detail_service_category_id_foreign` FOREIGN KEY (`service_category_id`) REFERENCES `service_category` (`service_category_id`);
+  ADD CONSTRAINT `proposals_detail_service_category_id_foreign` FOREIGN KEY (`service_category_id`) REFERENCES `service_category` (`service_category_id`),
+  ADD CONSTRAINT `proposals_detail_proposal_id_foreign` FOREIGN KEY (`proposal_id`) REFERENCES `proposals` (`proposal_id`);
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_sale_attachment_id_foreign` FOREIGN KEY (`sale_attachment_id`) REFERENCES `sale_attachment` (`sale_attachment_id`),
+  ADD CONSTRAINT `sales_proposal_id_foreign` FOREIGN KEY (`proposal_id`) REFERENCES `proposals` (`proposal_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
